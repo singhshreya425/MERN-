04bdb17c947b1926f04d9bd12cdf052982bfe4d2
shreya <singhshreay425@gmail.com>
2023-03-24 09:05:41 +0530

    new

---
+++ virtualalbumCS/vaserver/src/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const route = require("./routes/route")
+const app = express()
+require("dotenv").config()
+const mongoose = require("mongoose")
+const multer=require("multer")
+app.use(express.json())
+app.use(multer().any())
+
+app.use(express.urlencoded({ extended: true }))
+mongoose.set('strictQuery', true)//Deprication error
+mongoose.connect(process.env.DB,
+    { useNewUrlParser: true })
+    .then(() => console.log(("MongoDb is connected")))
+    .catch(err => console.log(err.message))
+
+app.use("/", route)
+
+app.listen(process.env.PORT , function () {
+    console.log("Express is running on port " + (process.env.PORT ))
+})
---
+++ virtualalbumCS/virtualalbum/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eonline.com%2Fnews%2F570396%2Ftaylor-swift-reveals-latest-single-and-announces-new-album-1989-watch-the-music-video&psig=AOvVaw29vrGbpyU-KiiLAdxYixfZ&ust=1679509079742000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLiZ_4LR7f0CFQAAAAAdAAAAABAL"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
